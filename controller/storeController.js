777b954f2fffefe1f7f78c0d37569042963eff21
adityamudgil2505 <adityamudgil2505@gmail.com>
2019-01-18 13:24:36 +0530

    Done all controller folder

--- controller/storeController.js
+++ controller/storeController.js
@@ -2,8 +2,8 @@ const postDB = require('../database/models/Posts');
 const path = require('path');
 module.exports = (req,res)=>{
   const {image} = req.files;
-  console.log('../'+__dirname,'public/posts/', image.name);
-  image.mv(path.resolve('../public/posts/', image.name),(err)=>{
+  console.log();
+  image.mv(path.resolve(__dirname,'../public/posts/', image.name),(err)=>{
     postDB.create({
       ...req.body,
       image:'/posts/'+image.name
