c636d9f1a8f29687973e5763deb74f97ef226b5c
adityamudgil2505 <adityamudgil2505@gmail.com>
2019-01-19 23:56:24 +0530

    Added hashing
    using bcrypt

--- controller/storeUserController.js
+++ controller/storeUserController.js
@@ -1,5 +1,9 @@
 const user = require('../database/models/user');
 module.exports = (req, res)=>{
-  user.create(req.body,(err,post)=>{});
-  res.redirect('/');
+  user.create(req.body,(err,post)=>{
+    if(err)
+    { return res.redirect('/auth/register');
+    }
+    res.redirect('/');
+  });
 };
--- database/models/user.js
+++ database/models/user.js
@@ -1,9 +1,25 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
+  username: 
+  { type: String,
+    required: true,
+    unique: true
+  },
+  email: 
+  { type: String,
+    required: true,
+    unique: true
+  },
   password: String
 });
 
-/// save cmd  userSchema.
+userSchema.pre('save',function(next){
+  const user = this;
+  bcrypt.hash(user.password,10,function(err,encrypted){
+    user.password=encrypted;
+    next();
+  });
+});
+
 module.exports = mongoose.model('user',userSchema);
